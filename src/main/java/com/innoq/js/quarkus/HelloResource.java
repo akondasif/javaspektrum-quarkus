4a8e545b583a24a974a3dfbd9ae09a5c946b2d30
Michael Vitz <michael.vitz@innoq.com>
2019-04-17 07:23:47 +0200

    Try JSON

--- src/main/java/com/innoq/js/quarkus/HelloResource.java
+++ src/main/java/com/innoq/js/quarkus/HelloResource.java
@@ -4,13 +4,28 @@ import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("/hello")
 public class HelloResource {
 
     @GET
-    @Produces(MediaType.TEXT_PLAIN)
-    public String hello() {
-        return "Hello, World!";
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response hello() {
+        Greeting greeting = new Greeting("Hello, World!");
+        return Response.ok(greeting).build();
+    }
+
+    public static class Greeting {
+
+        private String text;
+
+        public Greeting(String text) {
+            this.text = text;
+        }
+
+        public String getText() {
+            return text;
+        }
     }
 }
