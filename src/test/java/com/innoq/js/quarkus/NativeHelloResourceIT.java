71e2af2c7293621a34f37c0c31783c0a7613d415
Michael Vitz <michael.vitz@innoq.com>
2019-04-17 06:27:21 +0200

    Initial commit

---
+++ src/test/java/com/innoq/js/quarkus/NativeHelloResourceIT.java
@@ -0,0 +1,9 @@
+package com.innoq.js.quarkus;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeHelloResourceIT extends HelloResourceTest {
+
+    // Execute the same tests but in native mode.
+}
